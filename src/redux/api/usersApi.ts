72c6c6bd86d7561d7c44aa33f8fb9429975aa434
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2023-10-18 20:00:38 +0600

    usersApi added

---
+++ src/redux/api/usersApi.ts
@@ -0,0 +1,54 @@
+import { IMeta, IUser } from "@/types";
+import { TagTypes } from "../tag-types";
+import { baseApi } from "./baseApi";
+
+const USER_URL = "/users";
+export const usersApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    // get all users
+    users: build.query({
+      query: (arg: Record<string, any>) => {
+        return {
+          url: `${USER_URL}`,
+          method: "GET",
+          params: arg,
+        };
+      },
+      transformResponse: (response: IUser[], meta: IMeta) => {
+        return {
+          users: response,
+          meta,
+        };
+      },
+      providesTags: [TagTypes.user],
+    }),
+    // get single user
+    user: build.query({
+      query: (id: string | string[] | undefined) => ({
+        url: `${USER_URL}/${id}`,
+        method: "GET",
+      }),
+      providesTags: [TagTypes.user],
+    }),
+
+    // update user
+    updateUser: build.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/${data.id}`,
+        method: "PATCH",
+        data: data.body,
+      }),
+      invalidatesTags: [TagTypes.user],
+    }),
+    // delete user
+    deleteUser: build.mutation({
+      query: (id) => ({
+        url: `${USER_URL}/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [TagTypes.user],
+    }),
+  }),
+});
+
+export const {} = usersApi;
