a04a8576729ee27d88b473ecebc9bb24deec475d
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2023-10-18 20:17:50 +0600

    packageApi added in redux api

---
+++ src/redux/api/packageApi.ts
@@ -0,0 +1,59 @@
+import { IMeta, IPackage } from "@/types";
+import { TagTypes } from "../tag-types";
+import { baseApi } from "./baseApi";
+
+const PACKAGE_URL = "/packages";
+export const packageApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    // get all packages
+    packages: build.query({
+      query: (arg: Record<string, any>) => {
+        return {
+          url: `${PACKAGE_URL}`,
+          method: "GET",
+          params: arg,
+        };
+      },
+      transformResponse: (response: IPackage[], meta: IMeta) => {
+        return {
+          packages: response,
+          meta,
+        };
+      },
+      providesTags: [TagTypes.package],
+    }),
+    // get single package
+    package: build.query({
+      query: (id: string | string[] | undefined) => ({
+        url: `${PACKAGE_URL}/${id}`,
+        method: "GET",
+      }),
+      providesTags: [TagTypes.package],
+    }),
+
+    // update package
+    updatePackage: build.mutation({
+      query: (data) => ({
+        url: `${PACKAGE_URL}/${data.id}`,
+        method: "PATCH",
+        data: data.body,
+      }),
+      invalidatesTags: [TagTypes.package],
+    }),
+    // delete package
+    deletePackage: build.mutation({
+      query: (id) => ({
+        url: `${PACKAGE_URL}/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [TagTypes.package],
+    }),
+  }),
+});
+
+export const {
+  usePackagesQuery,
+  usePackageQuery,
+  useDeletePackageMutation,
+  useUpdatePackageMutation,
+} = packageApi;
--- src/redux/tag-types.ts
+++ src/redux/tag-types.ts
@@ -1,5 +1,21 @@
 export enum TagTypes {
   user = "user",
+  package = "packages",
+  newConnectionReq = "newConnectionReq",
+  customerConnectionStatusRoutes = "customerConnectionStatus",
+  payment = "payment",
+  review = "review",
+  notification = "notification",
+  blog = "blog",
 }
 
-export const TagTypesList = [TagTypes.user];
+export const TagTypesList = [
+  TagTypes.user,
+  TagTypes.package,
+  TagTypes.newConnectionReq,
+  TagTypes.customerConnectionStatusRoutes,
+  TagTypes.payment,
+  TagTypes.review,
+  TagTypes.notification,
+  TagTypes.blog,
+];
