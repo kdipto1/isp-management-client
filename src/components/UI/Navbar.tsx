65f8a44ae1b2019abf53427dfe17afbf006eddc9
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2023-11-03 13:17:28 +0600

    navbar menu drawer added for mobile devices

--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -2,53 +2,89 @@
 
 import { navbarItems } from "@/constants/navbarItems";
 import { useAppSelector } from "@/redux/hooks";
-import { HomeOutlined, MenuFoldOutlined } from "@ant-design/icons";
-import { ConfigProvider, Layout, Menu } from "antd";
+import {
+  HomeOutlined,
+  MenuFoldOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+import { Button, Col, ConfigProvider, Drawer, Layout, Menu, Row } from "antd";
 import Link from "next/link";
+import { useState } from "react";
 
 const { Header } = Layout;
 
 const Navbar = () => {
   const role = useAppSelector((state) => state.userRole.role);
+  const [visible, setVisible] = useState(false);
 
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
   return (
-    <>
+    <Layout>
       <Header
         style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          backgroundColor: "transparent",
-          paddingRight: "10px",
+          padding: 0,
         }}
       >
-        <Link href={"/"} style={{}}>
-          <HomeOutlined style={{ fontSize: "30px" }} />
-        </Link>
-        <ConfigProvider
-          theme={{
-            components: {
-              Menu: {
-                itemActiveBg: "none",
-                // itemSelectedColor: "black",
-                colorBgBlur: "#00b96b",
-                colorBgBase: "transparent",
-              },
-            },
-          }}
+        <Row justify={"space-between"} align={"middle"}>
+          <Col xs={20} sm={20} md={4}>
+            <Link href={"/"} style={{}}>
+              <HomeOutlined style={{ fontSize: "30px" }} />
+            </Link>
+          </Col>
+          <Col xs={0} sm={0} md={20}>
+            <ConfigProvider
+              theme={{
+                components: {
+                  Menu: {
+                    itemActiveBg: "none",
+                    // itemSelectedColor: "black",
+                    colorBgBlur: "#00b96b",
+                    colorBgBase: "transparent",
+                  },
+                },
+              }}
+            >
+              <Menu
+                multiple={true}
+                theme="light"
+                overflowedIndicator={<MenuFoldOutlined />}
+                disabledOverflow
+                mode="horizontal"
+                defaultSelectedKeys={["2"]}
+                items={navbarItems(role)}
+              />
+            </ConfigProvider>
+          </Col>
+          <Col xs={2} sm={2} md={0}>
+            <Button type="primary" onClick={showDrawer}>
+              <MenuOutlined />
+            </Button>
+          </Col>
+        </Row>
+
+        {/*  */}
+        <Drawer
+          title="Menu"
+          placement="right"
+          onClick={onClose}
+          onClose={onClose}
+          open={visible}
         >
           <Menu
-            multiple={true}
-            theme="dark"
-            overflowedIndicator={<MenuFoldOutlined />}
-            disabledOverflow
-            mode="horizontal"
+            theme="light"
+            mode="vertical"
             defaultSelectedKeys={["2"]}
             items={navbarItems(role)}
           />
-        </ConfigProvider>
+        </Drawer>
       </Header>
-    </>
+    </Layout>
   );
 };
 
