e3efe4c1387c8eb461c55f983bc6ab73c379e90d
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2023-11-03 12:31:48 +0600

    banner ui changed

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -1,47 +1,75 @@
-import { Carousel } from "antd";
+import { Carousel, Col, Row } from "antd";
 import Image from "next/image";
 import banner2 from "../../assets/banner2.jpeg";
 import banner1 from "../../assets/banner1.jpg";
 import banner from "../../assets/banner.jpg";
 
 const contentStyle: React.CSSProperties = {
-  height: "700px",
+  height: "600px",
   color: "#fff",
   lineHeight: "160px",
   textAlign: "center",
   background: "#364d79",
+  clipPath: `ellipse(50% 50% at 50% 50%)`,
 };
 
-const Banner: React.FC = () => (
-  <Carousel autoplay>
-    <div>
-      <Image
-        style={contentStyle}
-        src={banner2}
-        width={1500}
-        height={1000}
-        alt="banner image"
-      />
-    </div>
-    <div>
-      <Image
-        style={contentStyle}
-        src={banner}
-        width={1600}
-        height={400}
-        alt="banner image"
-      />
-    </div>
-    <div>
-      <Image
-        style={contentStyle}
-        src={banner1}
-        width={1600}
-        height={400}
-        alt="banner image"
-      />
-    </div>
-  </Carousel>
-);
+const Banner = () => {
+  return (
+    <section style={{ margin: "20px 0" }}>
+      <Row gutter={2}>
+        <Col
+          span={12}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "1.5rem",
+          }}
+        >
+          <h2>
+            Get <span style={{ color: "#00b96b" }}>Steady and Stable</span>
+            <span style={{ display: "block" }}>
+              Internet Experience With Us
+            </span>
+          </h2>
+        </Col>
+        <Col span={12}>
+          <Carousel autoplay>
+            <div>
+              <div style={contentStyle}>
+                <Image
+                  src={banner2}
+                  width={1500}
+                  height={700}
+                  alt="banner image"
+                />
+              </div>
+            </div>
+            <div>
+              <div style={contentStyle}>
+                <Image
+                  src={banner}
+                  width={1600}
+                  height={700}
+                  alt="banner image"
+                />
+              </div>
+            </div>
+            <div>
+              <div style={contentStyle}>
+                <Image
+                  src={banner1}
+                  width={1600}
+                  height={700}
+                  alt="banner image"
+                />
+              </div>
+            </div>
+          </Carousel>
+        </Col>
+      </Row>
+    </section>
+  );
+};
 
 export default Banner;
--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import { navbarItems } from "@/constants/navbarItems";
-
 import { useAppSelector } from "@/redux/hooks";
-
-import { BankFilled, HomeOutlined, MenuFoldOutlined } from "@ant-design/icons";
-import { Layout, Menu } from "antd";
+import { HomeOutlined, MenuFoldOutlined } from "@ant-design/icons";
+import { ConfigProvider, Layout, Menu } from "antd";
 import Link from "next/link";
 
 const { Header } = Layout;
@@ -20,35 +18,35 @@ const Navbar = () => {
           display: "flex",
           alignItems: "center",
           justifyContent: "space-between",
-          // backgroundColor: "wheat",
+          backgroundColor: "transparent",
           paddingRight: "10px",
         }}
       >
         <Link href={"/"} style={{}}>
           <HomeOutlined style={{ fontSize: "30px" }} />
         </Link>
-        {/* <ConfigProvider
+        <ConfigProvider
           theme={{
             components: {
               Menu: {
                 itemActiveBg: "none",
-                itemSelectedColor: "black",
+                // itemSelectedColor: "black",
+                colorBgBlur: "#00b96b",
+                colorBgBase: "transparent",
               },
             },
           }}
-        > */}
-
-        <Menu
-          multiple={true}
-          theme="dark"
-          overflowedIndicator={<MenuFoldOutlined />}
-          disabledOverflow
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          items={navbarItems(role)}
-        />
-
-        {/* </ConfigProvider> */}
+        >
+          <Menu
+            multiple={true}
+            theme="dark"
+            overflowedIndicator={<MenuFoldOutlined />}
+            disabledOverflow
+            mode="horizontal"
+            defaultSelectedKeys={["2"]}
+            items={navbarItems(role)}
+          />
+        </ConfigProvider>
       </Header>
     </>
   );
